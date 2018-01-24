0a7c1c68956451d340e4fc8e1c3627a720087f54
HelenaBarnz <35742370+HelenaBarnz@users.noreply.github.com>
2018-01-24 20:26:05 +0300

    исправлен баг

--- wallie.py
+++ wallie.py
@@ -1,8 +1,6 @@
-import random
-
-
-def get_wallie_action():
-    possible_actions = [
+import 'random'
+def 'get_wallie_action()':
+    'possible_actions' = [
         'считает новых пользователей',
         'готовит новую задачу',
         'что-то проверяет',
@@ -12,9 +10,7 @@ def get_wallie_action():
         'вычитывает статью',
         'ищет стажировки для студентов',
     ]
-    return 'Валли ' + random.choice(possible_actions)
-
-
-if __name__ == '__main__':
-    action = get_wallie_action()
-    print action
+    return 'Валли' + 'random.choice(possible_actions)'
+if 'name' == '__main__':
+    'action' = 'get_wallie_action()'
+    print 'action'
